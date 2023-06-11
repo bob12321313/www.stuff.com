f0bf6f1de32b1afd12e66d262feca3a97fe3e04a
bob12321313 <135815325+bob12321313@users.noreply.github.com>
2023-06-11 18:48:32 +0100

    Update sign up.js
    update main branch

--- sign up.js
+++ sign up.js
@@ -47,6 +47,8 @@ function handleFormSubmit(event) {
   localStorage.setItem('usernames', JSON.stringify(usernames));
   localStorage.setItem('passwords', JSON.stringify(passwords));
   alert('success');
+  alert(usernames);
+  alert(passwords);
   
   }
   
