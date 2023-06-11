24e6a1138ce74dce52e12ecc2938bbca0f9e2ccc
bob12321313 <135815325+bob12321313@users.noreply.github.com>
2023-06-11 08:29:49 +0100

    Update sign up.js
    update main branch

--- sign up.js
+++ sign up.js
@@ -1,6 +1,7 @@
 // Define empty arrays to store usernames and passwords
 let usernames = [];
 let passwords = [];
+let names = [];
 
 // Retrieve usernames and passwords from local storage if they exist
 if (localStorage.getItem('usernames')) {
@@ -11,23 +12,31 @@ if (localStorage.getItem('passwords')) {
   passwords = JSON.parse(localStorage.getItem('passwords'));
 }
 
+if (localStorage.getItem('names')) {
+  names = JSON.parse(localStorage.getItem('names'));
+}
+
 // Function to handle form submission
 function handleFormSubmit(event) {
   event.preventDefault(); // Prevent form submission
 
   // Get the entered username and password values
+  const nameInput = document.getElementById('name');
   const usernameInput = document.getElementById('username');
   const passwordInput = document.getElementById('password');
 
+  const name = nameInput.value;
   const username = usernameInput.value;
   const password = passwordInput.value;
 
   // Add the username and password to the arrays
+  names.push(name);
   usernames.push(username);
   passwords.push(password);
 
   
   // Save the updated arrays to local storage
+  localStorage.setItem('names', JSON.stringify(names));
   localStorage.setItem('usernames', JSON.stringify(usernames));
   localStorage.setItem('passwords', JSON.stringify(passwords));
   alert('success');
@@ -35,13 +44,14 @@ function handleFormSubmit(event) {
 
   
   // Clear the form inputs
+  nameInput.value = '';
   usernameInput.value = '';
   passwordInput.value = '';
 
  
 }
 
-// Add an event listener to the f
+
 document.addEventListener('DOMContentLoaded', function() {
   const signupForm = document.getElementById('signupForm');
   signupForm.addEventListener('submit', handleFormSubmit);
