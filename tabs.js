a6c136b0c94966d933c615736005afc23cc31a02
bob12321313 <135815325+bob12321313@users.noreply.github.com>
2023-06-14 17:05:08 +0100

    Update tabs.js
    update main branch

--- tabs.js
+++ tabs.js
@@ -17,11 +17,69 @@ function selectUnit(event) {
 
 // Function to initiate the battle
 function initiateBattle() {
-  // Print the selected units for testing
-  console.log("Army 1 Units:", selectedUnitsArmy1);
-  console.log("Army 2 Units:", selectedUnitsArmy2);
+  if (selectedUnitsArmy1.length === 0 || selectedUnitsArmy2.length === 0) {
+    alert("Please select units for both armies.");
+    return;
+  }
 
-  // Add your battle logic here
+  const battleResultContainer = document.getElementById("battle-result");
+  battleResultContainer.innerHTML = ""; // Clear previous battle result
+
+  const battleResult = simulateBattle(selectedUnitsArmy1, selectedUnitsArmy2);
+  const winner = battleResult.winner;
+  const army1Survivors = battleResult.army1Survivors;
+  const army2Survivors = battleResult.army2Survivors;
+
+  let resultText = "Battle Result: ";
+  if (winner === "draw") {
+    resultText += "It's a draw!";
+  } else {
+    resultText += `Army ${winner} wins!`;
+  }
+
+  resultText += `<br>Army 1 Survivors: ${army1Survivors}<br>Army 2 Survivors: ${army2Survivors}`;
+
+  battleResultContainer.innerHTML = resultText;
+}
+
+// Function to simulate the battle
+function simulateBattle(army1, army2) {
+  const army1Survivors = [];
+  const army2Survivors = [];
+
+  const maxRounds = Math.max(army1.length, army2.length);
+
+  for (let i = 0; i < maxRounds; i++) {
+    const army1Unit = army1[i % army1.length];
+    const army2Unit = army2[i % army2.length];
+
+    const outcome = determineOutcome(army1Unit, army2Unit);
+
+    if (outcome === "army1") {
+      army1Survivors.push(army1Unit);
+    } else if (outcome === "army2") {
+      army2Survivors.push(army2Unit);
+    }
+  }
+
+  let winner = "draw";
+  if (army1Survivors.length > army2Survivors.length) {
+    winner = "1";
+  } else if (army2Survivors.length > army1Survivors.length) {
+    winner = "2";
+  }
+
+  return {
+    winner: winner,
+    army1Survivors: army1Survivors.join(", "),
+    army2Survivors: army2Survivors.join(", "),
+  };
+}
+
+// Function to randomly determine the outcome of a battle round
+function determineOutcome(unit1, unit2) {
+  const outcomes = ["army1", "army2", "draw"];
+  return outcomes[Math.floor(Math.random() * outcomes.length)];
 }
 
 // Add event listeners to the unit list items
